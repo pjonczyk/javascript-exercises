6ed4cf019c4685cc85c680f0038bb5a33bcfa8c1
Philipp Jonczyk <philjonczyk@gmail.com>
2022-04-27 21:16:10 +0200

    finished fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function (number) {
+  number = parseInt(number);
+  if (number < 0) {
+    return "OOPS";
+  }
+  let first = 1;
+  let second = 1;
+  let nthnumber = 2;
+  while (nthnumber < number) {
+    let buf = second;
+    second = first + second;
+    first = buf;
+    nthnumber++;
+  }
+  return second;
 };
 
 // Do not edit below this line

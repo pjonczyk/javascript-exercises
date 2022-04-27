ae21d8fdda8b1d12ca266ac26808c4f9e4dde4c8
Philipp Jonczyk <philjonczyk@gmail.com>
2022-04-27 20:57:14 +0200

    finished palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+  text = text.replaceAll(",", "");
+  text = text.replaceAll(".", "");
+  text = text.replaceAll("!", "");
+  text = text.replaceAll(" ", "");
+  text = text.toLowerCase();
+  const reversed = [...text].reverse().join("");
+  return reversed == text;
 };
 
 // Do not edit below this line
